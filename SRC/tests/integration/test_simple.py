5ea549523a8f6c17c6298d99ed047a84f50fe549
Xejb <vladiq-artamonenko@rambler.ru>
2022-09-27 21:52:13 +0300

    fix tests

--- SRC/tests/integration/test_simple.py
+++ SRC/tests/integration/test_simple.py
@@ -1,8 +1,9 @@
+"""Method."""
 def test_simple_request_home(client):
     resp = client.get("/")
     assert resp.text == "Hello"
 
 
 def test_add_pos(client):
-    resp = client.get("/add?a=1&=9")
-    assert resp.text == '10'
+    resp = client.get("/add?a=1&b=9")
+    assert resp.text == "10"
